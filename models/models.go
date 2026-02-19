a730db22380ddde1f03540554991bdf6e954f2da
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-20 06:13:07 +0700

    feat: add struct ItemKeranjang and method

--- models/models.go
+++ models/models.go
@@ -8,10 +8,21 @@ type Menu struct {
 	Harga int    `json:"harga"`
 }
 
+type ItemKeranjang struct {
+	Nama   string
+	Harga  int
+	Jumlah int
+}
+
 type Tampilable interface {
 	Tampilkan()
 }
 
 func (m Menu) Tampilkan() {
 	fmt.Printf("ID: %d | %s - Rp%d\n", m.Id, m.Name, m.Harga)
+}
+
+func (i ItemKeranjang) Tampilkan() {
+	subtotal := i.Harga * i.Jumlah
+	fmt.Printf(" %s x%d = Rp%d\n", i.Nama, i.Jumlah, subtotal)
 }
